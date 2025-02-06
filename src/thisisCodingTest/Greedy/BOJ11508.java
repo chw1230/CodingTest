e5ad6aa63e9f35da40a21de3c4d923d57864e5b8
chw1230 <choihw1230@naver.com>
2025-02-06 15:02:49 +0900

    코딩테스트 BOJ11508 문제 풀기

---
+++ src/thisisCodingTest/Greedy/BOJ11508.java
@@ -0,0 +1,27 @@
+package src.thisisCodingTest.Greedy;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BOJ11508 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] arr = new Integer[n];
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if ((i+1) % 3 == 0) {
+                sum -= arr[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
