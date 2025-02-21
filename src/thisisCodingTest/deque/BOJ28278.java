e4b795d67afc0bc53bd04de381416617d3700295
chw1230 <choihw1230@naver.com>
2025-02-21 10:35:58 +0900

    BOJ28278 풀기

---
+++ src/thisisCodingTest/deque/BOJ28278.java
@@ -0,0 +1,55 @@
+package src.thisisCodingTest.deque;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BOJ28278 {
+
+    public static void main(String[] args) throws IOException {
+        Deque<String> deque = new ArrayDeque<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String[] c = br.readLine().split(" ");
+
+            switch (c[0]) {
+                case "1":
+                    deque.push(c[1]);
+                    break;
+
+                case "2":
+                    if (deque.isEmpty()) {
+                        System.out.println("-1");
+                    } else {
+                        System.out.println(deque.pop());
+
+                    }
+                    break;
+
+                case "3":
+                    System.out.println(deque.size());
+                    break;
+
+                case "4":
+                    if (deque.isEmpty()) {
+                        System.out.println("1");
+                    } else {
+                        System.out.println("0");
+                    }
+                    break;
+
+                case "5":
+                    if (deque.isEmpty()) {
+                        System.out.println("-1");
+                    } else {
+                        System.out.println(deque.peek());
+                    }
+            }
+        }
+    }
+}
