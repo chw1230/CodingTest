d219e9249f4f4c148ca5b33afc4c546c9fa2b090
chw1230 <choihw1230@naver.com>
2025-03-05 18:08:37 +0900

    BOJ 2164 문제 풀기

---
+++ src/thisisCodingTest/sort/BOJ2164.java
@@ -0,0 +1,27 @@
+package src.thisisCodingTest.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BOJ2164 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 1; i <= N; i++) {
+            deque.offer(i);
+        }
+
+        while (deque.size() != 1) {
+            deque.poll();
+            int move = deque.poll();
+            deque.offer(move);
+        }
+        System.out.println(deque.poll());
+    }
+}
