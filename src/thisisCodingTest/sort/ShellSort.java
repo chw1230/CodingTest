ac420d8f3eab97a519aa64b122062adfe4d8d13e
chw1230 <choihw1230@naver.com>
2025-04-01 21:14:16 +0900

    shell sort 구현

--- src/thisisCodingTest/sort/ShellSort.java
+++ src/thisisCodingTest/sort/ShellSort.java
@@ -1,4 +1,4 @@
-package src.thisisCodingTest.sort;
+package thisisCodingTest.sort;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -29,7 +29,10 @@ public class ShellSort {
 
         for (int gap = len / 2; 0 < gap; gap = gap / 2) {
             for (int i = gap; i < len; i++) {
-                for (int j = i - gap; 0 <= j && arr[j+gap] < arr[j]; j = j - gap) {
+                for (int j = i - gap; 0 <= j && arr[j+gap] < arr[j]; j = j - gap) { // gap 만큼의 격차를 가진 인덱스 값을 비교
+                    // A --gap-- B
+                    // i와 gap의 차이가 0보다 커야하고, 좌측에 있는 값이 더 큰 경우(정렬이 안되어 있는 경우) 바꾸기
+                    // 그리고 j의 값은 또 gap 만큼 작는 인덱스와 비교하게 하기 위해 gap 만큼을 빼주기
                     int tmp = arr[j];
                     arr[j] = arr[j + gap];
                     arr[j + gap] = tmp;
