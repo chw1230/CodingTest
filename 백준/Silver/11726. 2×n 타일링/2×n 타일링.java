c71f844aa9085254dc23619a4dd8ade92c4b802a
CHOI HYUN WOO <101390526+chw1230@users.noreply.github.com>
2025-07-17 18:57:07 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n + 1];
+        arr[1] = 1;
+         if ( n == 1 ) { // 기본으로 정해준 값이 나온 경우 먼저 출력해주기!! -> 오답 이유
+            System.out.println(1);
+            return;
+        }
+        arr[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            arr[i] = (arr[i-1] + arr[i-2]) % 10007;
+        }
+        System.out.println(arr[n]);
+    }
+}
