406ccda0012240b39fda0ada1ff88d58499599b2
CHOI HYUN WOO <101390526+chw1230@users.noreply.github.com>
2025-09-13 17:48:51 +0900

    [Silver I] Title: 어두운 건 무서워, Time: 676 ms, Memory: 80900 KB -BaekjoonHub

---
+++ 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken()); // 알아볼 평균 개수
+
+        int arr[][] = new int[R + 1][C + 1];
+        for (int i = 1; i <= R; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= C; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken()) + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1]; // 중복 빼주기!
+            }
+        }
+
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int r1 = Integer.parseInt(st.nextToken());
+            int c1 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+
+            int sum = arr[r2][c2] - arr[r2][c1 - 1] - arr[r1 - 1][c2] + arr[r1 - 1][c1 - 1];
+            int cnt = (r2 - r1 + 1) * (c2 - c1 + 1);
+            System.out.println(sum/cnt);
+        }
+    }
+}
