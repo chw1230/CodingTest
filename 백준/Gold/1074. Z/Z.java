ccf74d6c5f9d9b0f76d442665106f1cf1227d72a
CHOI HYUN WOO <101390526+chw1230@users.noreply.github.com>
2025-10-27 09:04:03 +0900

    [Gold V] Title: Z, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -4,46 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    // 메모리 초과가 뜬다... -> 배열을 만들지 말고 풀어보자....ㅜㅜ
-    // 배열에 -1 넣고 -1 인 애들만 값을 바꾸는 과정으로 문제 풀었음 -> 메모리 초과
-    // 그러면 어떻게 배열 안쓰고 하지? -> 분할 과정에서 4조각 중 자기에 해당되는 것으로 가서 걔만 계산해서 구하는 방식으로 구함!
-    private static int r,c;
-
-    private static int div(int y, int x, int size) {
-        if (size / 2 <= 0) {
-            return 0;
-        }
-
-        if ( 0 <= y && y < size/2 && 0 <= x && x < size/2 ) {
-//            System.out.println("좌측 상단");
-            // 좌측 상단
-            return (size / 2) * (size / 2) * 0 + div(y, x, size / 2);
-
-        } else if ( 0 <= y && y < size/2 &&  size/2 <= x && x < size ) {
-//            System.out.println("우측 상단");
-            // 우측 상단
-            return (size / 2) * (size / 2) * 1 + div(y, x - size / 2, size / 2);
-
-        } else if ( size/2 <= y && y < size && 0 <= x && x < size/2 ) {
-//            System.out.println("좌측 하단");
-            // 촤측 하단
-            return (size / 2) * (size / 2) * 2 + div(y - size / 2, x, size / 2);
-
-        } else {
-//            System.out.println("우측 하단");
-            // 우측 하단
-            return (size / 2) * (size / 2) * 3 + div(y - size / 2, x - size / 2, size / 2);
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken()); // r행
-        c = Integer.parseInt(st.nextToken()); // c열
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
         System.out.println(div(r, c, (int) Math.pow(2,N)));
+    }
+
+    private static int div(int r, int c, int size) {
+        if (size == 1) {
+            return 0;
+        }
+
+        int h = size / 2;
+        if (r < h && c < h) {
+            return div(r, c, h);
+
+        } else if (r < h && c >= h) {
+            return h * h * 1 + div(r, c - h, h);
+
+        } else if (r >= h && c < h) {
+            return h * h * 2 + div(r - h, c, h);
+
+        } else {
+            return h * h * 3 + div(r - h, c - h, h);
+
+        }
     }
 }
