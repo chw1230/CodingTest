35978b1156a18441b3a2a01e6d013b09d148b396
CHOI HYUN WOO <101390526+chw1230@users.noreply.github.com>
2025-11-15 09:46:06 +0900

    Title: 삼각형 컨베이어 벨트, Time: 87ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 251115/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < 3*N; i++) {
+            if (i % 3 == 0) {
+                st = new StringTokenizer(br.readLine());
+            }
+            dq.addLast(Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i = 0; i < T; i++) {
+            int right = dq.removeLast();
+            dq.addFirst(right);
+        }
+
+        for (int i = 0; i < N*3; i++) {
+            System.out.print(dq.removeFirst()+ " ");
+            if ( i != 0 && (i+1) % 3 == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
