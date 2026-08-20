8fa66bb38c0f6e7f87ee2d04f8de6583da92661a
CHOI HYUN WOO <101390526+chw1230@users.noreply.github.com>
2026-08-20 13:21:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.00 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,52 +1,52 @@
 import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Queue;
 
 class Solution {
-    private static int[] moveX = {-1, 0, 1, 0};
-    private static int[] moveY = {0, -1, 0, 1};
-    private static int[][] dis; // 최단 거리를 담을 배열
-    private static Deque<int[]> q;
+    
+    static int[] moveX = {-1, 0, 1, 0};
+    static int[] moveY = {0, -1, 0, 1};
+    
+    public static int solution(int[][] maps) {
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
 
-    public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
+        bfs(visited, maps);
 
-        dis = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                dis[i][j] = -1;
-            }
+        int answer;
+        if (maps[maps.length - 1][maps[0].length - 1] == 1) {
+            answer = -1;
+        } else {
+            answer = maps[maps.length - 1][maps[0].length - 1];
         }
+        return answer;
+    }
 
-        if (maps[0][0] == 0) {
-            return -1;
-        }
+    private static void bfs(boolean[][] visited, int[][] maps) {
+        Queue<int[]> queue = new ArrayDeque<>();
 
-        q = new ArrayDeque<>();
-        dis[0][0] = 1; // 시작점의 최단 거리 1으로 처리하기
-        q.addLast(new int[]{0, 0}); // y,x
+        queue.add(new int[]{0, 0}); // 시작점 좌상단
+        visited[0][0] = true; // 시작점 방문 처리
 
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int y = cur[0];
-            int x = cur[1];
-
-            if (y == n - 1 && x == m - 1) {
-                return dis[y][x];
-            }
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int curX = cur[0];
+            int curY = cur[1];
 
             for (int i = 0; i < 4; i++) {
-                int newX = x + moveX[i];
-                int newY = y + moveY[i];
+                // 다음 이동할 좌표
+                int nextX = curX + moveX[i];
+                int nextY = curY + moveY[i];
 
-                if ( -1 < newX && newX < m && -1 < newY && newY < n) {
-                    if (maps[newY][newX] == 1 && dis[newY][newX] == -1) { // 길 이고 최단거리 갱신이 없는 경우에만
-                        dis[newY][newX] = dis[y][x] + 1;
-                        q.addLast(new int[]{newY, newX});
-                    }
+                // 범위 벗어나면 패스
+                if (nextX < 0 || nextY < 0 || nextX >= maps.length || nextY >= maps[0].length) {
+                    continue;
+                }
+
+                if (!visited[nextX][nextY] && maps[nextX][nextY] == 1) {
+                    queue.add(new int[]{nextX, nextY}); // q에 넣기
+                    visited[nextX][nextY] = true; // 방문 처리
+                    maps[nextX][nextY] = maps[curX][curY] + 1;
                 }
             }
         }
-        return -1;
     }
 }
