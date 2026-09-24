e57e7ec20c8aba2c96b8e0d353cd3e3e435e33dd
CHOI HYUN WOO <101390526+chw1230@users.noreply.github.com>
2026-09-24 20:48:08 +0900

    [level 2] Title: 구명보트, Time: 14.90 ms, Memory: 59.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,21 +1,21 @@
 import java.util.Arrays;
 
 class Solution {
-    public static int solution(int[] people, int limit) {
+      public static int solution(int[] people, int limit) {
         int answer = 0;
 
         Arrays.sort(people);
-        int i = 0; // 가장 가벼운 사람을 가리키는 idx
-        int j = people.length - 1; //  가장 무거운 사람을 가리키는 idx
+        int l = 0;
+        int h = people.length - 1;
 
-        while (i <= j) {
-            if (people[i] + people[j] <= limit) {
-                i++; // 다음 가벼운 사람으로 넘어가기
-            }
-            j--; // 덜 무거운 사람으로 넘어가기
-            answer++; // case 1 : if 만족해서 가벼운 사람과 무거운 사람 짝지어 지는 경우 / case2 : if 만족하지 않아서 무거운 사람만 보트에 담는 경우
+        while (l <= h) {
+            int sum = people[l] + people[h];
+            if (sum <= limit) {
+                l++;
+            } 
+            h--;
+            answer++;
         }
-
         return answer;
-    }
+      }
 }
